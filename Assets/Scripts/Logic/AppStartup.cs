88dcb12573c39087fc373eb4f3a4e0dcc0e707f7
Filipe Rodrigues <liperodri750@gmail.com>
2026-04-21 10:09:51 +0100

    Fix things

--- Assets/Scripts/Logic/AppStartup.cs
+++ Assets/Scripts/Logic/AppStartup.cs
@@ -3,9 +3,25 @@ using UnityEngine;
 public class AppStartup : MonoBehaviour
 {
     [SerializeField] StateMachine m_StateMachine;
+    private bool m_IsRunning;
  [ContextMenu("StartTut")]
 private void StartTutorial()
     {
         m_StateMachine.Enter();
+        m_IsRunning = true;
+    }
+
+
+    
+    void Update()
+    {
+
+        if (m_IsRunning)
+        {
+            m_StateMachine.Execute();
+
+            if (m_StateMachine.IsFinished())
+                m_IsRunning = false;
+        }
     }
 }
