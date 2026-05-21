5e9ebb055998eefb00e12d38bc597daa1ca276a6
Filipe Rodrigues <liperodri750@gmail.com>
2026-05-21 16:51:32 +0100

    fixed compression logic and added haptic feedback

--- Assets/Scripts/CPR/CPRSoftLockFollower.cs
+++ Assets/Scripts/CPR/CPRSoftLockFollower.cs
@@ -13,11 +13,53 @@ public class CPRSoftLockFollower : MonoBehaviour
     [SerializeField] private float rotationFollowSpeed = 18f;
     [SerializeField] private float snapPositionDistance = 0.01f;
     [SerializeField] private float snapRotationDegrees = 8f;
+    [SerializeField] private bool resetWhenUnaligned = true;
+    [SerializeField] private bool resetToSourceTransform = true;
+    [SerializeField] private float resetPositionSpeed = 12f;
+    [SerializeField] private float resetRotationSpeed = 16f;
+
+    [Header("Reset Pose Override")]
+    [SerializeField] private bool useResetPoseOverride;
+    [SerializeField] private Vector3 resetLocalPosition;
+    [SerializeField] private Vector3 resetLocalEuler;
+    [SerializeField] private bool useResetWorldPoseOverride;
+    [SerializeField] private Vector3 resetWorldPosition;
+    [SerializeField] private Vector3 resetWorldEuler;
+    [SerializeField] private bool useFixedLocalReset = true;
+    [SerializeField] private Vector3 fixedLocalPosition = new Vector3(0f, 0f, -0.05f);
+    [SerializeField] private Vector3 fixedLocalEuler = new Vector3(0f, 180f, 0f);
+
+    private Vector3 initialLocalPosition;
+    private Quaternion initialLocalRotation;
+    private bool hasInitialPose;
 
     private void Awake()
     {
         if (proxyRoot == null)
             proxyRoot = transform;
+
+        if (useFixedLocalReset)
+        {
+            initialLocalPosition = fixedLocalPosition;
+            initialLocalRotation = Quaternion.Euler(fixedLocalEuler);
+        }
+        else if (useResetWorldPoseOverride)
+        {
+            initialLocalPosition = proxyRoot.localPosition;
+            initialLocalRotation = proxyRoot.localRotation;
+        }
+        else if (useResetPoseOverride)
+        {
+            initialLocalPosition = resetLocalPosition;
+            initialLocalRotation = Quaternion.Euler(resetLocalEuler);
+        }
+        else
+        {
+            initialLocalPosition = proxyRoot.localPosition;
+            initialLocalRotation = proxyRoot.localRotation;
+        }
+
+        hasInitialPose = true;
     }
 
     private void LateUpdate()
@@ -29,7 +71,12 @@ public class CPRSoftLockFollower : MonoBehaviour
             return;
 
         if (followOnlyWhenAligned && !placementDetector.IsAligned)
+        {
+            if (resetWhenUnaligned)
+                ResetToInitialPose();
+
             return;
+        }
 
         float alignmentStrength = Mathf.Clamp01(placementDetector.AlignmentProgress);
         float positionT = Mathf.Clamp01(Time.deltaTime * positionFollowSpeed * Mathf.Max(0.15f, alignmentStrength));
@@ -44,5 +91,72 @@ public class CPRSoftLockFollower : MonoBehaviour
             proxyRoot.rotation = targetRotation;
         else
             proxyRoot.rotation = Quaternion.Slerp(proxyRoot.rotation, targetRotation, rotationT);
+    }
+
+    private void OnDisable()
+    {
+        if (proxyRoot == null)
+            return;
+
+        if (useFixedLocalReset)
+        {
+            proxyRoot.localPosition = fixedLocalPosition;
+            proxyRoot.localRotation = Quaternion.Euler(fixedLocalEuler);
+            return;
+        }
+
+        if (useResetWorldPoseOverride)
+        {
+            proxyRoot.position = resetWorldPosition;
+            proxyRoot.rotation = Quaternion.Euler(resetWorldEuler);
+            return;
+        }
+
+        if (useResetPoseOverride)
+        {
+            proxyRoot.localPosition = resetLocalPosition;
+            proxyRoot.localRotation = Quaternion.Euler(resetLocalEuler);
+            return;
+        }
+
+        if (hasInitialPose)
+        {
+            proxyRoot.localPosition = initialLocalPosition;
+            proxyRoot.localRotation = initialLocalRotation;
+        }
+    }
+
+    private void ResetToInitialPose()
+    {
+        if (!hasInitialPose || proxyRoot == null)
+            return;
+
+        float positionT = Mathf.Clamp01(Time.deltaTime * resetPositionSpeed);
+        float rotationT = Mathf.Clamp01(Time.deltaTime * resetRotationSpeed);
+
+        if (useFixedLocalReset)
+        {
+            proxyRoot.localPosition = fixedLocalPosition;
+            proxyRoot.localRotation = Quaternion.Euler(fixedLocalEuler);
+            return;
+        }
+
+        if (resetToSourceTransform && placementDetector != null && placementDetector.SourceTransform != null)
+        {
+            var source = placementDetector.SourceTransform;
+            proxyRoot.position = Vector3.Lerp(proxyRoot.position, source.position, positionT);
+            proxyRoot.rotation = Quaternion.Slerp(proxyRoot.rotation, source.rotation, rotationT);
+            return;
+        }
+
+        if (useResetWorldPoseOverride)
+        {
+            proxyRoot.position = Vector3.Lerp(proxyRoot.position, resetWorldPosition, positionT);
+            proxyRoot.rotation = Quaternion.Slerp(proxyRoot.rotation, Quaternion.Euler(resetWorldEuler), rotationT);
+            return;
+        }
+
+        proxyRoot.localPosition = Vector3.Lerp(proxyRoot.localPosition, initialLocalPosition, positionT);
+        proxyRoot.localRotation = Quaternion.Slerp(proxyRoot.localRotation, initialLocalRotation, rotationT);
     }
 }
